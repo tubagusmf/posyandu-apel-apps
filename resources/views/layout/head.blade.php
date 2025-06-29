42e7690e63343ce7c5fa9ac0e8b09636b730b17a
tubagusmf <tubagusfadillah98@gmail.com>
2025-06-30 02:44:15 +0700

    Refactor title in authentication views for Kader and Bidan accounts

--- resources/views/layout/head.blade.php
+++ resources/views/layout/head.blade.php
@@ -2,7 +2,16 @@
 <html lang="en">
   <head>
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <title>Dashboard - Kader</title>
+    <title>
+      Dashboard -
+      @if(Auth::guard('kader')->check())
+        Kader
+      @elseif(Auth::guard('bidan')->check())
+        Bidan
+      @else
+        Posyandu
+      @endif
+    </title>    
     <meta
       content="width=device-width, initial-scale=1.0, shrink-to-fit=no"
       name="viewport"
