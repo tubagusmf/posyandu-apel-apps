38251107132d07bcbc1ff3bec0c659ef18097979
tubagusmf <tubagusfadillah98@gmail.com>
2025-08-23 14:49:20 +0700

    Show total number of Balita and Ibu Hamil data in dashboards

--- app/Http/Controllers/Kader/DashboardKaderController.php
+++ app/Http/Controllers/Kader/DashboardKaderController.php
@@ -5,16 +5,33 @@ namespace App\Http\Controllers\Kader;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\Anak;
+use App\Models\IbuHamil;
+use Carbon\Carbon;
 
 class DashboardKaderController extends Controller
 {
     public function index()
     {
         $kader = Auth::guard('kader')->user();
+        
+        $now = Carbon::now();
+
+        $balita0_12 = Anak::whereRaw('TIMESTAMPDIFF(MONTH, tgl_lahir, ?) BETWEEN 0 AND 12', [$now])->count();
+        $balita13_36 = Anak::whereRaw('TIMESTAMPDIFF(MONTH, tgl_lahir, ?) BETWEEN 13 AND 36', [$now])->count();
+        $balita37_60 = Anak::whereRaw('TIMESTAMPDIFF(MONTH, tgl_lahir, ?) BETWEEN 37 AND 60', [$now])->count();
+
+        $ibuHamil = IbuHamil::count();
+
         $data = [
             'title' => 'Dashboard Kader',
-            'content' => 'kader/dashboard'
+            'content' => 'kader/dashboard',
+            'balita0_12' => $balita0_12,
+            'balita13_36' => $balita13_36,
+            'balita37_60' => $balita37_60,
+            'ibuHamil' => $ibuHamil,
         ];
+
         return view('layout.wrapper', compact('kader'), $data);
     }
 }
--- resources/views/kader/dashboard.blade.php
+++ resources/views/kader/dashboard.blade.php
@@ -9,6 +9,93 @@
                 <h4 class="op-7 mb-2">Hi, {{ $kader->nama_kader }} Welcome To Dashboard Kader Posyandu Apel</h4>
               </div>
             </div>
+
+            <div class="row">
+              <div class="col-sm-6 col-md-3">
+                <div class="card card-stats card-round">
+                  <div class="card-body">
+                    <div class="row align-items-center">
+                      <div class="col-icon">
+                        <div
+                          class="icon-big text-center icon-danger bubble-shadow-small"
+                        >
+                          <i class="fas fa-users"></i>
+                        </div>
+                      </div>
+                      <div class="col col-stats ms-3 ms-sm-0">
+                        <div class="numbers">
+                          <p class="card-category">Balita usia 0-12 Bulan</p>
+                          <h4 class="card-title">{{ $balita0_12 }}</h4>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm-6 col-md-3">
+                <div class="card card-stats card-round">
+                  <div class="card-body">
+                    <div class="row align-items-center">
+                      <div class="col-icon">
+                        <div
+                          class="icon-big text-center icon-warning bubble-shadow-small"
+                        >
+                          <i class="fas fa-users"></i>
+                        </div>
+                      </div>
+                      <div class="col col-stats ms-3 ms-sm-0">
+                        <div class="numbers">
+                          <p class="card-category">Balita usia 13-36 Bulan</p>
+                          <h4 class="card-title">{{ $balita13_36 }}</h4>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm-6 col-md-3">
+                <div class="card card-stats card-round">
+                  <div class="card-body">
+                    <div class="row align-items-center">
+                      <div class="col-icon">
+                        <div
+                          class="icon-big text-center icon-primary bubble-shadow-small"
+                        >
+                          <i class="fas fa-users"></i>
+                        </div>
+                      </div>
+                      <div class="col col-stats ms-3 ms-sm-0">
+                        <div class="numbers">
+                          <p class="card-category">Balita usia 37-60 Bulan</p>
+                          <h4 class="card-title">{{ $balita37_60 }}</h4>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm-6 col-md-3">
+                <div class="card card-stats card-round">
+                  <div class="card-body">
+                    <div class="row align-items-center">
+                      <div class="col-icon">
+                        <div
+                          class="icon-big text-center icon-success bubble-shadow-small"
+                        >
+                          <i class="fas fa-users"></i>
+                        </div>
+                      </div>
+                      <div class="col col-stats ms-3 ms-sm-0">
+                        <div class="numbers">
+                          <p class="card-category">Ibu Hamil usia 1-42 Minggu</p>
+                          <h4 class="card-title">{{ $ibuHamil }}</h4>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
             
           </div>
         </div>
